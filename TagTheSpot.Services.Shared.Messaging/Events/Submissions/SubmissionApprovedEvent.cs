9c457b088fbd51aacefc48c5c4b514705133cfb8
Savchenko Artem <p.savchenkoad.back@gmail.com>
2025-07-31 17:22:05 +0300

    [TTS-77] Add Submission Events (#3)
    https://psavchenkoad.atlassian.net/browse/TTS-77?atlOrigin=eyJpIjoiODA4MjQ2MTllYWVmNGNjYjkzY2Q0YTE2NWI4NTBhYzIiLCJwIjoiaiJ9

---
+++ TagTheSpot.Services.Shared.Messaging/Events/Submissions/SubmissionApprovedEvent.cs
@@ -0,0 +1,7 @@
+﻿using TagTheSpot.Services.Shared.Messaging.Events.Abstractions;
+
+namespace TagTheSpot.Services.Shared.Messaging.Events.Submissions
+{
+    public sealed record SubmissionApprovedEvent(
+        Guid SubmissionId) : EventBase;
+}
