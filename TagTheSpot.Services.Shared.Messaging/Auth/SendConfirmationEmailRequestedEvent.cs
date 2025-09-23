6d1a8a48189e2bfdaeab350c5435286c71c1a736
savchenkoaddev <p.savchenkoad.dev@gmail.com>
2025-09-23 15:41:54 +0300

    Add the event

---
+++ TagTheSpot.Services.Shared.Messaging/Auth/SendConfirmationEmailRequestedEvent.cs
@@ -0,0 +1,6 @@
+﻿namespace TagTheSpot.Services.Shared.Messaging.Auth
+{
+    public sealed record SendConfirmationEmailRequestedEvent(
+        string Recipient,
+        string ConfirmationLink);
+}
