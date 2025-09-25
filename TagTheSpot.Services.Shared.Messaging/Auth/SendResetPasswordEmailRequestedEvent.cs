6d96f321c2e8851049337cf7455b876f8b457100
Savchenko Artem <p.savchenkoad.back@gmail.com>
2025-09-25 08:12:52 +0300

    [TTS-189] Implement Forgot Password Endpoint (#10)
    https://psavchenkoad.atlassian.net/browse/TTS-189?atlOrigin=eyJpIjoiYWRlYmFkM2ZmZDFmNDZiOThkY2VkODM3ZjZmN2QyNzciLCJwIjoiaiJ9

---
+++ TagTheSpot.Services.Shared.Messaging/Auth/SendResetPasswordEmailRequestedEvent.cs
@@ -0,0 +1,6 @@
+﻿namespace TagTheSpot.Services.Shared.Messaging.Auth
+{
+    public sealed record SendResetPasswordEmailRequestedEvent(
+        string Recipient,
+        string ResetPasswordLink);
+}
