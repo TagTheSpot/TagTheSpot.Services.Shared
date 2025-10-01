0334d41c098e4062f97a6898edcebd5e2223c407
savchenkoaddev <p.savchenkoad.dev@gmail.com>
2025-10-01 17:37:15 +0300

    Extract functionality

---
+++ TagTheSpot.Services.Shared.API/DependencyInjection/SwaggerExtensions.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Net.Http.Headers;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace TagTheSpot.Services.Shared.API.DependencyInjection
+{
+    public static class SwaggerExtensions
+    {
+        public static IServiceCollection ConfigureSwaggerGen(
+            this IServiceCollection services,
+            string swaggerXmlFilePath)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                ConfigureSwaggerAuthorization(options);
+
+                options.IncludeXmlComments(swaggerXmlFilePath);
+            });
+
+            return services;
+        }
+
+        private static void ConfigureSwaggerAuthorization(SwaggerGenOptions options)
+        {
+            options.ConfigureAppSecurityDefinitions();
+            options.ConfigureAppSecurityRequirements();
+        }
+
+        private static SwaggerGenOptions ConfigureAppSecurityDefinitions(this SwaggerGenOptions options)
+        {
+            options.AddSecurityRequirement(
+                new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = JwtBearerDefaults.AuthenticationScheme
+                            }
+                        },
+                        new string[] { }
+                    }
+                });
+
+            return options;
+        }
+
+        private static SwaggerGenOptions ConfigureAppSecurityRequirements(this SwaggerGenOptions options)
+        {
+            options.AddSecurityDefinition(
+                JwtBearerDefaults.AuthenticationScheme,
+                new OpenApiSecurityScheme
+                {
+                    Name = HeaderNames.Authorization,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = JwtBearerDefaults.AuthenticationScheme,
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer { token }\""
+                });
+
+            return options;
+        }
+    }
+}
